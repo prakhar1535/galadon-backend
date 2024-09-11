10273c679f7f622409818c3bfff53e16007b1e68
prakhar1535 <prakharsharma1535@gmail.com>
2024-09-11 21:04:50 +0530

    build fix

--- __init__.py
+++ __init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from db.db import init_db
 from routes import register_apps
-from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
     init_db(app)
     register_apps(app)
 
