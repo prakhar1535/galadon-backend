63b05f7762d99f9a1676966841e6a9fd6371cce2
prakhar1535 <prakharsharma1535@gmail.com>
2024-09-13 01:51:52 +0530

    build fix

--- __init__.py
+++ __init__.py
@@ -5,19 +5,10 @@ from routes import register_apps
 
 def create_app():
     app = Flask(__name__)
-    
-    # Configure CORS
-    CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
-    
-    # Initialize database
+    CORS(app)
     init_db(app)
-    
-    # Register routes
     register_apps(app)
-
     return app
-
-# If you want to run the app directly from this file
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
--- gunicorn.sh
+++ gunicorn.sh
@@ -5,6 +5,6 @@ source venv/bin/activate
 
 pip install --upgrade pip
 
-pip install --use-pep517 -r  ./requirements.txt --force-reinstall
+pip install --no-cache-dir --use-pep517 -r  ./requirements.txt --force-reinstall
 
 gunicorn -c gunicorn.conf.py
